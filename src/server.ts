9640bfec30ab5ebe9a36a8ed52edcc3f8739f957
borgesfran497@gmail.com <team@fastcode.com.br>
2022-08-13 20:58:18 -0300

    :feat: client usecases and controllers created

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,8 @@
 import express from "express";
+import { routes } from "./routes";
 
 const app = express();
-
-app.get("/", (req, res) => {
-  return res.json("running")
-})
+app.use(express.json());
+app.use(routes);
 
 app.listen(3000);
