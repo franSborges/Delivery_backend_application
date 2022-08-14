2bb1a97084fe48959a61b75cf57cbdf1b56faf8b
borgesfran988@gmil.com <team@fastcode.com.br>
2022-08-13 21:02:41 -0300

    :feat: client usecases and controllers created

--- src/routes.ts
+++ src/routes.ts
@@ -2,9 +2,7 @@ import { Router } from "express";
 import { CreateClientController } from "./modules/clients/useCases/createClient/CreateClientController";
 
 const routes = Router();
-
 const createClientController = new CreateClientController();
-
 routes.post("/client/", createClientController.handle);
 
 export { routes };
