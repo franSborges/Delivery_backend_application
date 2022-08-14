f65a809a17d09d18da5488a3eca691b3c36699d6
franSborges <borgesfran988@gmail.com>
2022-08-14 19:57:35 -0300

    :feat: handling exceptions

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,20 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import { routes } from "./routes";
+import "express-async-errors";
 
 const app = express();
 app.use(express.json());
 app.use(routes);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof Error) return res.status(400).json({
+    message: err.message
+  });
+
+  return res.status(500).json({
+    status: "error",
+    message: "Internal server error"
+  });
+})
 
 app.listen(3000);
